7da5d2c6b76b826bf8bc890459f17948dbe3bd99
sihwan0Lee <stay136@gmail.com>
2021-04-21 10:45:29 +0900

    dfs_hole

--- graph/DFS/2.01hole.py
+++ graph/DFS/2.01hole.py
@@ -14,9 +14,11 @@ for i in range(n):
 def dfs(x, y):
     if x <= -1 or x >= n or y <= -1 or y >= m:
         return False
-
+    # 아직 방문을 안했다면
     if graph[x][y] == 0:
+        # 해당 노드 방문 처리
         graph[x][y] = 1
+        # 상하좌우 위치도 모두 재귀적으로 호출
         dfs(x - 1, y)
         dfs(x, y - 1)
         dfs(x + 1, y)
@@ -25,6 +27,7 @@ def dfs(x, y):
     return False
 
 
+# 모든 노드(위치)에 대하여 음료수 채우기
 result = 0
 for i in range(n):
     for j in range(m):
