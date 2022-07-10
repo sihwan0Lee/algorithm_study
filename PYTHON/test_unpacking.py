b4533a23979e53c4a216d25d0f57535e3bd6fa50
sihwan0Lee <stay136@gmail.com>
2022-07-10 15:50:46 +0900

    010

--- PYTHON/test_unpacking.py
+++ PYTHON/test_unpacking.py
@@ -5,4 +5,11 @@ def sum(a, b, c):
 numbers = [1, 2, 3]
 #sum(numbers) # error
 
-print(sum(*numbers)) # 출력 : 6
+print(sum(*numbers)) # 출력 : 
+
+# 위치인자를 unpacking할때는 위에 예에서는 list타입이였지만, Container객체라면 다 가능합니다.
+
+print(sum(*'abc')) # 'abc'
+print(sum(*(4, 5, 6))) # 15
+print(sum(*{'가', '나', '다'})) # '나다가'
+print(sum(*{'치킨': 3, '피자': 12, '음료수': 10})) # '치킨피자음료수'
