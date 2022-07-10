f19025546d1d9735dce2f0b1be3602e9190ae9ac
sihwan0Lee <stay136@gmail.com>
2022-07-10 15:52:03 +0900

    0710

--- PYTHON/test_unpacking.py
+++ PYTHON/test_unpacking.py
@@ -13,3 +13,23 @@ print(sum(*'abc')) # 'abc'
 print(sum(*(4, 5, 6))) # 15
 print(sum(*{'가', '나', '다'})) # '나다가'
 print(sum(*{'치킨': 3, '피자': 12, '음료수': 10})) # '치킨피자음료수'
+
+#동일한 방식으로 키워드인자로 unpacking할 수 있습니다. unpacking하기 위해선 인자가 key와 인자로 구성되어 있는 mapping타입, 즉 dict가 필요합니다.
+
+def cal(first, op, second):
+    if op == '+':
+        return first + second
+    if op == '/':
+        return first / second
+    if op == '-':
+        return first - second
+    if op == '*':
+        return first * second
+
+prob = {
+  'first': 12,
+  'second': 34,
+  'op': '*'
+}
+
+cal(**prob) # 결과 : 408
